0c87bbe9629f31b05ffe5ab066e4f82ece2602d1
DouglasOR <douglasor@outlook.com>
2018-01-17 09:05:49 -0200

    metodos

--- Carro.php
+++ Carro.php
@@ -6,4 +6,20 @@ Class Carro{
     public $ano;
     public $marca;
     public $motor;
+
+    public function getCor(){
+        return $this->cor;
+    }
+    
+    public function getAno(){
+        return $this->ano;
+    }
+
+    public function getMarca(){
+        return $this->marca;
+    }
+
+    public function getMotor(){
+        return $this->motor();
+    }
 }
--- index.php
+++ index.php
@@ -3,8 +3,10 @@ include_once("Carro.php");
 
 $mercedes = new Carro();
 $mercedes->cor = "preto";
-$mercedes->ano = 2016;
+$mercedes->ano = "2016";
 $mercedes->motor = 1.8;
 $mercedes->marca = "mercedes benz";
+
+echo $mercedes->getMarca()." do ano de:".$mercedes->getAno();
 
 var_dump($mercedes);
