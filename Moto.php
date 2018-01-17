2961fbe5d0e01d1be73ac4ef8fe0af0043dd5a82
DouglasOR <douglasor@outlook.com>
2018-01-17 20:58:38 -0200

    calling parent

--- Moto.php
+++ Moto.php
@@ -2,4 +2,9 @@
 
 class Moto extends Veiculo{
 
+    public function __construct($motor,$cor){
+
+        parent::__construct($motor,strtoupper($cor));
+    }
+
 }
--- index.php
+++ index.php
@@ -3,21 +3,21 @@ include_once("Carro.php");
 include_once("Moto.php");
 
 $mercedes = new Carro("1.8","preto");
-
 $mercedes->setAno("2016");
-
 $mercedes->setMarca("mercedes benz")  ;
 $mercedes->portas = 2;
 
 $bis = new Moto(1.8,"preto" );
-
 $bis->setAno("2016");
-
 $bis->setMarca("honda")  ;
 
-echo $mercedes->getMarca()." do ano de:".$mercedes->getAno()."<br>";
+
+echo $mercedes->getMarca()." do ano de: ".$mercedes->getAno()."<br>";
 
 
-echo $bis->getMarca()." do ano de:".$bis->getAno();
+echo $bis->getMarca()." do ano de: ".$bis->getAno()."<br>";
+
+echo $bis->cor;
+
 var_dump($mercedes);
 var_dump($bis);
